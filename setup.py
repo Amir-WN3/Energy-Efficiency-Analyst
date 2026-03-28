ce3b648cc0f63bb2c0a15d737684bb4923ffba7f
Amir Nassar <amirnassar5555@gmail.com>
2026-03-28 12:10:35 +0100

    Refactor: move files to new layout, remove processors folder, add packaging files

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function returns the list of requirements
+    '''
+    requirements = []
+    HYPHEN_E_DOT = '-e .'
+
+    with open(file_path, 'r') as file:
+        requirements = file.readlines()
+        requirements = [req.replace('\n','') for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+setup(
+    name = 'energy-efficiency-analyst',
+    version = '0.0.1',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt'),
+)
